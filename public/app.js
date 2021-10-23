1e63d078c62eaf35bd0b23002e464d3012b122e0
symplycharles <maduabuchi.charles@yahoo.com>
2021-10-23 17:21:16 +0300

    first commit

---
+++ public/app.js
@@ -0,0 +1,116 @@
+const messageTypes = { LEFT: 'left', RIGHT: 'right', LOGIN: 'login' };
+
+//Chat stuff
+const chatWindow = document.getElementById('chat');
+const messagesList = document.getElementById('messagesList');
+const messageInput = document.getElementById('messageInput');
+const sendBtn = document.getElementById('sendBtn');
+
+//login stuff
+let username = '';
+const usernameInput = document.getElementById('usernameInput');
+const loginBtn = document.getElementById('loginBtn');
+const loginWindow = document.getElementById('login');
+
+const messages = []; // { author, date, content, type }
+
+//Connect to socket.io - automatically tries to connect on same port app was served from
+var socket = io();
+
+socket.on('message', message => {
+	//Update type of message based on username
+	if (message.type !== messageTypes.LOGIN) {
+		if (message.author === username) {
+			message.type = messageTypes.RIGHT;
+		} else {
+			message.type = messageTypes.LEFT;
+		}
+	}
+
+	messages.push(message);
+	displayMessages();
+
+	//scroll to the bottom
+	chatWindow.scrollTop = chatWindow.scrollHeight;
+});
+
+createMessageHTML = message => {
+	if (message.type === messageTypes.LOGIN) {
+		return `
+			<p class="secondary-text text-center mb-2">${
+				message.author
+			} joined the chat...</p>
+		`;
+	}
+	return `
+	<div class="message ${
+		message.type === messageTypes.LEFT ? 'message-left' : 'message-right'
+	}">
+		<div class="message-details flex">
+			<p class="flex-grow-1 message-author">${
+				message.type === messageTypes.LEFT ? message.author : ''
+			}</p>
+			<p class="message-date">${message.date}</p>
+		</div>
+		<p class="message-content">${message.content}</p>
+	</div>
+	`;
+};
+
+displayMessages = () => {
+	const messagesHTML = messages
+		.map(message => createMessageHTML(message))
+		.join('');
+	messagesList.innerHTML = messagesHTML;
+};
+
+sendBtn.addEventListener('click', e => {
+	e.preventDefault();
+	if (!messageInput.value) {
+		return console.log('Invalid input');
+	}
+
+	const date = new Date();
+	const month = ('0' + (date.getMonth()+1)).slice(0, 2);
+	const year = date.getFullYear();
+	const day = date.getDate();
+	const dateString = `${day}-${month}-${year}`;
+
+	const message = {
+		author: username,
+		date: dateString,
+		content: messageInput.value
+	};
+	sendMessage(message);
+	//clear input
+	messageInput.value = '';
+});
+
+loginBtn.addEventListener('click', e => {
+	e.preventDefault();
+	if (!usernameInput.value) {
+		return console.log('Must supply a username');
+	}
+
+	//set the username and create logged in message
+	username = usernameInput.value;
+	sendMessage({ author: username, type: messageTypes.LOGIN });
+
+	//show chat window and hide login
+	loginWindow.classList.add('hidden');
+	chatWindow.classList.remove('hidden');
+});
+
+// Fired on Persist.js initialization
+$(document).on('added.ac.persist', function (e) {
+	var storageKey = [e.key.element, e.key.type, e.key.path].join('.');
+	var value = mutationSession.get(storageKey);
+  
+	if (value) $(e.target).persist('update', e.key, value);
+  });
+
+
+
+sendMessage = message => {
+	socket.emit('message', message);
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+var PORT = process.env.PORT || 3000; // take port from heroku or for loacalhost
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+//Serve public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, +'public/index.html'));
+});
+
+io.on('connection', socket => {
+	console.log('a user connected');
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
+
+	socket.on('message', message => {
+		console.log('message: ' + message);
+		//Broadcast the message to everyone
+		io.emit('message', message);
+	});
+});
+http.listen(PORT, function() {
+	console.log("server started");
+});
